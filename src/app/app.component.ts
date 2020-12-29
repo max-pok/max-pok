a6ac61e6938b04cab8a75b8248d6682aca89c8f2
max-pok <maxim.p9@gmail.com>
2020-12-29 23:25:59 +0200

    add nav color change on scroll

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { NgwWowService } from 'ngx-wow';
+import { Component, HostListener, OnDestroy, OnInit } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+import { filter } from 'rxjs/operators';
+declare const test: any;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent  {
   title = 'max-pok';
+  isScrolled = false;
+
+  constructor(private router: Router, private wowService: NgwWowService) {
+    this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd))
+      .subscribe(() => {
+      this.wowService.init(); 
+    });
+  }
+
+  scroll(el: HTMLElement): void {
+    el.scrollIntoView({behavior: 'smooth'});
+  }
+
+  @HostListener("window:scroll", ['$event'])
+  scrollEvent($event: any) {
+    this.isScrolled = window.pageYOffset >= 80;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NgwWowModule } from 'ngx-wow';
+import { NgCircleProgressModule } from 'ng-circle-progress';
+
 
 @NgModule({
   declarations: [
@@ -10,7 +17,30 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FontAwesomeModule,
+    NgbModule,
+    NgCircleProgressModule.forRoot({
+      "radius": 50,
+      "space": -6,
+      "outerStrokeWidth": 6,
+      "outerStrokeColor": "#0bceaf",
+      "outerStrokeGradientStopColor": "#53a9ff",
+      "innerStrokeColor": "#828282",
+      "innerStrokeWidth": 6,
+      "titleColor": "#ebebeb",
+      "unitsColor": "#ebebeb",
+      "unitsFontSize": "20",
+      "animateTitle": false,
+      "showSubtitle": false,
+      "animationDuration": 2000,
+      "showTitle": true,
+      "showUnits": true,
+      "showBackground": true,
+      "clockwise": true,
+      "startFromZero": false,
+      })
   ],
   providers: [],
   bootstrap: [AppComponent]
